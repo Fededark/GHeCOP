719b9a3d205feafbf951d41cb83a37fd83b24c77
Federico Xella <xellafe@gmail.com>
2017-04-18 15:53:56 +0200

    Reworked user model

--- models/user.js
+++ models/user.js
@@ -1,12 +1,14 @@
 var moongose = require("mongoose");
 
 var userSchema = moongose.Schema({
-    key: String,
+    //key: String,
     firstName: String,
     lastName: String,
-    birthdate: Date,
+    birthdate: String,
+    hospital: String,
     email: String,
     password: String,
+    phone: String,
     created: { type: Date, default: Date.now },
     updated: { type: Date, default: Date.now }
 })
